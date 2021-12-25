24cc9c74ebdb66cbf03defa9a591239a96f2e305
Vaibhav Sompura <42895093+vsompura3@users.noreply.github.com>
2021-12-25 18:56:33 +0530

    Natours setup with tailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    colors: {
+      white: '#ffffff',
+      black: '#000000',
+      primary: '#55c57a',
+      'primary-light': '#7ed56f',
+      'primary-dark': '#28b485',
+      'secondary-light': '#ffb900',
+      'secondary-dark': '#ff7730',
+      'tertiary-light': '#2998ff',
+      'tertiary-dark': '#5643fa',
+      'grey-light-main': '#f7f7f7',
+      'grey-light-sub': '#eeeeee',
+      'grey-dark': '#777777',
+      'grey-dark-main': '#999999',
+      'grey-dark-sub': '#333333',
+    },
+    fontFamily: {
+      Lato: ['Lato', 'Arial', 'Helvetica', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
